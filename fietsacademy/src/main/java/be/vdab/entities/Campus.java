eed57df5429c9d29486924a11a9298fba9f735a7
Sander Vermeulen <sdimension@hotmail.com>
2015-04-09 13:19:36 +0200

    Na heropstarten computer

--- fietsacademy/src/main/java/be/vdab/entities/Campus.java
+++ fietsacademy/src/main/java/be/vdab/entities/Campus.java
@@ -1,14 +1,22 @@
 package be.vdab.entities;
 
 import java.io.Serializable;
+import java.util.Collections;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
+import javax.persistence.CollectionTable;
+import javax.persistence.ElementCollection;
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OrderBy;
 import javax.persistence.Table;
 
 import be.vdab.valueobjects.Adres;
+import be.vdab.valueobjects.TelefoonNr;
 
 @Entity
 @Table(name = "campussen")
@@ -20,13 +28,30 @@ public class Campus implements Serializable {
 	private String naam;
 	@Embedded
 	private Adres adres;
+	@ElementCollection
+	@CollectionTable(name = "campussentelefoonnrs", joinColumns = @JoinColumn(name = "campusid"))
+	@OrderBy("fax")
+	private Set<TelefoonNr> telefoonNrs;
 
 	public Campus(String naam, Adres adres) {
 		setNaam(naam);
 		setAdres(adres);
+		telefoonNrs = new LinkedHashSet<>();
 	}
 
 	protected Campus() {
+	}
+
+	public Set<TelefoonNr> getTelefoonNrs() {
+		return Collections.unmodifiableSet(telefoonNrs);
+	}
+
+	public void addTelefoonNr(TelefoonNr telefoonNr) {
+		telefoonNrs.add(telefoonNr);
+	}
+
+	public void removeTelefoonNr(TelefoonNr telefoonNr) {
+		telefoonNrs.remove(telefoonNr);
 	}
 
 	public long getId() {
